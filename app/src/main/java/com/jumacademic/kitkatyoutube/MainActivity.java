433851feff53bb0213af3be8e1b9352b3df67585
nepolionchakma <nepolionchakma.nc@gmail.com>
2022-11-19 16:06:42 +0100

    first git commit

---
+++ app/src/main/java/com/jumacademic/kitkatyoutube/MainActivity.java
@@ -0,0 +1,88 @@
+package com.jumacademic.kitkatyoutube;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
+import android.os.Build;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView txtView;
+    WebView myWebView;
+    String myUrl = "https://www.youtube.com/results?search_query=live+tv+bd";
+    String setTxt = "No Internet !!"+ "\n"+"ইন্টারনেট নেই !!";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        myWebView = findViewById(R.id.myWebView);
+        txtView = findViewById(R.id.txtView);
+
+        myWebView.getSettings().setJavaScriptEnabled(true);
+
+        myWebView.setWebViewClient(new WebViewClient());
+
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+
+        if (networkInfo!=null && networkInfo.isConnected() && networkInfo.isAvailable()){
+            myWebView.loadUrl(myUrl);
+
+
+        }
+
+
+        else {
+            txtView.setText(setTxt);
+        }
+
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (myWebView.canGoBack()) {
+            myWebView.goBack();
+        }else {
+
+            new AlertDialog.Builder(this)
+                    .setTitle("Want to Exit !!!!")
+                    .setMessage("If you Sure Click Yes and If Not click Cancel !!.")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                                finishAndRemoveTask();
+
+                            }
+
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                        }
+                    })
+                    .show();
+        }
+    }
+}
